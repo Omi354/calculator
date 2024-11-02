04bb58063eec860cd040bbc0663e5c2a97a8832b
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-11-03 02:33:31 +0900

    feat: 指定以外のインプットがあった場合のメッセージ出力処理を実装しました

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,57 @@
 package org.example;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    double firstInputNumber = 0;
 
-    System.out.println("1番目の数字を入力してください");
-    double firstInputNumber = scanner.nextInt();
+    // 1番目の数を設定する
+    for (; ; ) {
+      try {
+        System.out.println("1番目の数字を入力してください");
+        firstInputNumber = scanner.nextDouble();
+        break;
+      } catch (InputMismatchException e) {
+        e.printStackTrace();
+        System.out.println("数字のみ入力可能です");
+        scanner.next();
+      }
+    }
 
-    System.out.println("演算子を入力してください(+, -, *, /)");
-    String operator = scanner.next();
+    // 演算子を設定する
+    String operator = "";
 
-    System.out.println("2番目の数字を入力してください");
-    double secondInputNumber = scanner.nextInt();
+    for (; ; ) {
+      System.out.println("演算子を入力してください(+, -, *, /)");
+      operator = scanner.next();
+      if (operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals(
+          "/")) {
+        break;
+      } else {
+        System.out.println("+, -, *, /のみ入力可能です");
+      }
+    }
 
+    // 2番目の数を設定する
+
+    double secondInputNumber = 0;
+    for (; ; ) {
+      try {
+        System.out.println("2番目の数字を入力してください");
+        secondInputNumber = scanner.nextDouble();
+        break;
+      } catch (InputMismatchException e) {
+        e.printStackTrace();
+        System.out.println("数字のみ入力可能です");
+        scanner.next();
+      }
+    }
+
+    // 計算ロジック
     double result = 0;
 
     if (operator.equals("+")) {
