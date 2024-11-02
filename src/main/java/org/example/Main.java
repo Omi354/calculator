75eda2f0d5e9c3d287d2347231711ff45831b100
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-11-03 02:03:36 +0900

    feat: 計算のロジックを作成しました

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,34 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.printf("Hello and welcome!");
+    Scanner scanner = new Scanner(System.in);
 
+    System.out.println("1番目の数字を入力してください");
+    double firstInputNumber = scanner.nextInt();
+
+    System.out.println("演算子を入力してください(+, -, *, /)");
+    String operator = scanner.next();
+
+    System.out.println("2番目の数字を入力してください");
+    double secondInputNumber = scanner.nextInt();
+
+    double result = 0;
+
+    if (operator.equals("+")) {
+      result = firstInputNumber + secondInputNumber;
+    } else if (operator.equals("-")) {
+      result = firstInputNumber - secondInputNumber;
+    } else if (operator.equals("*")) {
+      result = firstInputNumber * secondInputNumber;
+    } else if (operator.equals("/")) {
+      result = firstInputNumber / secondInputNumber;
+    }
+
+    System.out.println(result);
 
   }
 }
